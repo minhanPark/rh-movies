f985e36e641735971532603a8b8fa78526fb94da
minhan <alsgks1218@gmail.com>
2020-01-12 17:49:58 +0900

    add navigation

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet, Text, View } from "react-native";
+import MainNavigation from "./navigation/MainNavigation";
 
 export default class App extends React.Component {
   state = {
@@ -23,9 +24,10 @@ export default class App extends React.Component {
     const { loaded } = this.state;
     if (loaded) {
       return (
-        <View style={styles.container}>
-          <Text>Open App!!</Text>
-        </View>
+        <>
+          <StatusBar barStyle="light-content" />
+          <MainNavigation />
+        </>
       );
     } else {
       return (
@@ -38,12 +40,3 @@ export default class App extends React.Component {
     }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
---
+++ navigation/TabNavigation.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Platform } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import TVScreen from "../screens/TV";
+import SearchScreen from "../screens/Search";
+import MoviesScreen from "../screens/Movies";
+import { BG_COLOR } from "../constants/Color";
+import TabBarIcon from "../components/TabBarIcon";
+import { createStack } from "./config";
+
+const TabNavigation = createBottomTabNavigator(
+  {
+    Movies: {
+      screen: createStack(MoviesScreen, "Movies"),
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+          />
+        )
+      }
+    },
+    TV: {
+      screen: createStack(TVScreen, "TV"),
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
+          />
+        )
+      }
+    },
+    Search: {
+      screen: createStack(SearchScreen, "Search"),
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-search" : "md-search"}
+          />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      style: {
+        backgroundColor: BG_COLOR
+      }
+    }
+  }
+);
+
+export default createAppContainer(TabNavigation);
