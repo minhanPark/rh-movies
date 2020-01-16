0fc1b5ef1a536763e05691fa2a21ca95def25909
minhan <alsgks1218@gmail.com>
2020-01-16 23:25:15 +0900

    movie slider completed

--- components/MovieSlide.js
+++ components/MovieSlide.js
@@ -1,6 +1,63 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import makePhotoUrl from "../utils/makePhotoUrl";
+import Layout from "../constants/Layout";
+import MoviePoster from "./MoviePoster";
+import MovieRating from "./MovieRating";
+import { TINT_COLOR } from "../constants/Color";
+
+const Container = styled.View`
+  flex: 1;
+  position: relative;
+`;
+
+const BgImage = styled.Image`
+  width: ${Layout.width};
+  height: ${Layout.height / 3};
+  opacity: 0.3;
+  position: absolute;
+`;
+
+const Content = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  padding-horizontal: 30px;
+  justify-content: space-between;
+`;
+
+const Column = styled.View`
+  width: 60%;
+  align-items: flex-start;
+`;
+
+const Title = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const Overview = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 12px;
+  margin-bottom: 10px;
+`;
+
+const VoteContainer = styled.View`
+  margin: 10px 0;
+`;
+
+const BtnContainer = styled.TouchableOpacity`
+  background-color: #e74c3c;
+  border-radius: 5px;
+  padding: 8px;
+`;
+
+const BtnText = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 12px;
+`;
 
 const MovieSlide = ({
   id,
@@ -9,7 +66,32 @@ const MovieSlide = ({
   title,
   voteAvg,
   overview
-}) => null;
+}) => (
+  <Container>
+    <BgImage source={{ uri: makePhotoUrl(backgroundPhoto) }} />
+    <Content>
+      <MoviePoster path={posterPhoto} />
+      <Column>
+        <Title>{title}</Title>
+        {voteAvg ? (
+          <VoteContainer>
+            <MovieRating votes={voteAvg} inSlide={true} />
+          </VoteContainer>
+        ) : null}
+        {overview ? (
+          <Overview>
+            {overview.length > 117
+              ? `${overview.substring(0, 117)}...`
+              : overview}
+          </Overview>
+        ) : null}
+        <BtnContainer>
+          <BtnText>View details</BtnText>
+        </BtnContainer>
+      </Column>
+    </Content>
+  </Container>
+);
 
 MovieSlide.propTypes = {
   id: PropTypes.number.isRequired,
